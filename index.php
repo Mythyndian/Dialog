739b7ff7a5ff2eb4f3675dca3bdc195e5e1156fa
Mythyndian <zawistowskimaciej1@gmail.com>
2019-07-15 18:01:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+    <title>Work in progres!</title>
+    <script  type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <script type="text/javascript">
+   
+    </script>
+  </head>
+  <body>  
+ 
+    
+   
+    <!-- Photo Big -->
+    <div class="img1">
+    <nav id="navigation" class="navbar navbar-expand-md  navbar-light sticky-top" >  
+    <!-- Brand -->
+          <a class="navbar-brand" href="#"><i class="fas fa-home"></i></a>
+        
+          <!-- Toggler/collapsibe Button -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <!-- Navbar links -->
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#about"><i class="fas fa-user"></i> ABOUT</a>
+              </li> 
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#gallery"><i class="fas fa-pen-square"></i> PORTFOLIO</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#contact"><i class="fas fa-paper-plane"></i> CONTACT</a>
+              </li>
+              
+              <li class="nav-item">
+                  <a class="nav-link" target="about_blank" href="https://github.com/Mythyndian"><i class="fab fa-github"></i> GITHUB</a>
+              </li>
+            </ul>
+          </div>
+        </nav> 
+        
+      </div>   
+    <div class="ptext">
+        <span class="border">MYTHYNDIAN </span>
+      </div>
+  </div>
+  <!-- Few words about me-->
+  <section id="about">
+  <div class="container about" >
+    <h3> ABOUT ME </h3>    
+  </div>
+  <div class="container about" ><i>I love photography</i></div>
+  <div class="container coll-sm-4">
+    We have created a fictional "personal" website/blog, and our fictional character is a hobby photographer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  <br/>
+  </div>
+  </section>
+</div>
+ <!--Big image 2-->
+<div class="img2">
+</div>
+  <!-- Gallery -->
+  <div class="container gallery" id="gallery">
+    <h4>Some works of my favorite instagram artist which i really recomend you to follow</h4>
+    <a href="images/lightbox/img1" data-lightbox="img1"><img src="images/lightbox/img1" alt="#"></a>
+    <a href="images/lightbox/img2" data-lightbox="img1"><img src="images/lightbox/img2" data-lightbox="img" alt="#"></a>
+    <a href="images/lightbox/img3" data-lightbox="img1"><img src="images/lightbox/img3" data-lightbox="img" alt="#"></a>
+    <a href="images/lightbox/img4" data-lightbox="img1"><img src="images/lightbox/img4" data-lightbox="img" alt="#"></a>
+    <a href="images/lightbox/img5" data-lightbox="img1"><img src="images/lightbox/img5" data-lightbox="img" alt="#"></a>
+    <a href="images/lightbox/img6" data-lightbox="img1"><img src="images/lightbox/img6" data-lightbox="img" alt="#"></a>
+    <a href="images/lightbox/img7" data-lightbox="img1"><img src="images/lightbox/img7" data-lightbox="img" alt="#"></a> 
+    <a href="images/lightbox/img8" data-lightbox="img1"><img src="images/lightbox/img8" data-lightbox="img" alt="#"></a>
+    
+  </div>
+  <!-- CONTACT-->
+  <div class="container-fluid bg-dark">
+    
+    <div class="container" id="contact">
+      <h3>SEND ME A MESSAGE</h3><br />
+      <form class="footer" method="POST" action="https://www.example.com/cgi-bin/FormMail.pl" accept-charset="utf-8" onsubmit="var originalCharset = document.charset;  document.charset = 'utf-8'; window.onbeforeunload = function () {document.charset=originalCharset;};"> Your Name <br /> 
+      <input name="realname" />
+       <br />
+        <label>Your Email</label> 
+        <br /> 
+        <input name="email" />
+         <br /> <label>Your Message</label> <br />
+          <textarea cols="25" rows="10" name="Message">
+
+          </textarea> <br /> <input type="submit" value="Send" />
+           <input type="hidden" name="recipient" value="admin@example.com" />
+            <input type="hidden" name="subject" value="Subject" />
+             <input type="hidden" name="redirect"  value="https://www.example.com/thanks.html" />
+              <input type="hidden" name="missing_fields_redirect"  value="https://www.example.com/error.html" />
+               <input type="hidden" name="required" value="realname,email,Message" />
+      </form>
+      </div>
+  </div>      
+    <!-- Optional JavaScript -->
+    <script src="/js/scrolling.js">
+    </script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
